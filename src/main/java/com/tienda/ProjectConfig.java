1695cf20a880685126f7a43ddf777cfdf6fc242c
jraygadaP <jpabloraygada@gmail.com>
2024-08-20 22:52:46 -0600

    - Push semana 13

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -23,6 +23,8 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
  
 @Configuration
@@ -132,5 +134,17 @@ public class ProjectConfig implements WebMvcConfigurer {
     }
     
     
+    
+     /* El siguiente método se utilizar para publicar en la nube, independientemente  */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    } 
 }
 
--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -5,26 +5,28 @@
 
 
 /* 
-Esta funciona carga lainfo de una imagen y la coloca en una etiqueta img
+ Esta funciona carga lainfo de una imagen y la coloca en una etiqueta img
  */
 
 
-function readURL(input){
-    if (input.file && input.files[0]){
+function readURL(input) {
+    if (input.file && input.files[0]) {
         var reader = new FileReader();
-        reader.onload = function (e){
-            $('#blah').attr('src',e.target.result.height(200));
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result.height(200));
         };
         reader.readAsDataURL(input.files[0]);
     }
-    
+
 }
 
-function addCart(formulario){
-    var idProducto=formulario.elements[0].value;
-    var existencias=formulario.elements[1].value;
-    if(existencias>0){
-        var url = "/carrito/agregar/"+idProducto;
+function addCart(formulario) {
+    var idProducto = formulario.elements[0].value;
+    var existencias = formulario.elements[1].value;
+    if (existencias > 0) {
+        var url = "/carrito/agregar/" + idProducto;
         $("#resultsBlock").load(url);
-    }
+        } else {
+        window.alert("No hay existencias");
+    }
 }
